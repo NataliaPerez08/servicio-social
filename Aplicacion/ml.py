f93ee4dda99f77b0e8e594374e94c76000900297
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2023-12-20 16:53:17 -0600

    Meter en funcion GNB

--- Aplicacion/ml.py
+++ Aplicacion/ml.py
@@ -1,26 +1,18 @@
-from cProfile import label
 from math import e
-from pyexpat import features
+from re import X
 import numpy as np
 import carbonatos.carbonato1 as c1
 import carbonatos.carbonato2 as c2
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score
 
 
 feature_names = ['wavelength','reflectance','pigmento','aglutinante','base','path','carpeta','tabla','espectro']
 
 ejemplares_c1 = c1.obtener_carbonato_C1()
 #ejemplares_c2 = c2.contenar_c2()
-
-#train, test=train_test_split(ejemplares_c1, test_size=0.3, random_state=0)
-
-#train, test, train_labels, test_labels = train_test_split(
-#   features,labels,test_size = 1, random_state = 22
-#)
-#X = [e['reflectance'] for e in ejemplares_c1]
 
 def encode_labels(label_names):
     label_names = list(label_names)
@@ -60,50 +52,11 @@ def etiquetar_ejemplares(labels,ejemplares):
             y.append(labels[aux])
     return y
 
-def gnb_p1_c1():
-    # Obtener features de TODOS los ejemplares
-    features = obtener_features(ejemplares_c1[0]) 
-    # Obtener etiquetas sin codificar de TODOS los ejemplares
-    get_labels = obtener_labels(ejemplares_c1[0])
-
-    # Codificación de las etiquetas
-    enco = encode_labels(get_labels)
-    y = etiquetar_ejemplares(enco,ejemplares_c1[0])
-    y = np.array(y)
-    X = np.array(features)
-
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
-
-
-    print(X_train.shape)
-    print(y_train.shape)
-
-    print(X_test.shape)
-    print(y_test.shape)
-
-    # Create Gaussian Naive Bayes object with prior probabilities of each
-    # label
-    GNBclf = GaussianNB(priors=None)
-    # Train model
-    model = GNBclf.fit(X, y)
-
-    # Make predictions
-    y_pred = GNBclf.predict(X_test)
-    print(y_pred)
-
-    # View accuracy score
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Precisión del modelo:", accuracy)
-
-    print(classification_report(y_test, y_pred))
-
-    #View confusion matrix
-    confusion_matrix(y_test, y_pred)
-
-def get_X_y_Tabla():
+# Función para obtener los datos de entrenamiento y prueba
+def get_X_y_Tabla(ejemplares):
     X = np.array([])
     y = np.array([])
-    for l in ejemplares_c1:
+    for l in ejemplares:
         features = obtener_features(l) 
         # Obtener etiquetas sin codificar de TODOS los ejemplares
         get_labels = obtener_labels(l)
@@ -118,8 +71,29 @@ def get_X_y_Tabla():
         X = np.concatenate((X,features),axis=0)
     return X,y
 
+# Función para obtener el modelo de Naive Bayes
+def get_GNB(ejemplares):
+    X,y = get_X_y_Tabla(ejemplares)
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
 
-X,y = get_X_y_Tabla()
+    # Create Gaussian Naive Bayes object with prior probabilities of each label
+    GNBclf = GaussianNB()
+    # Train model
+    model = GNBclf.fit(X, y)
 
-print(X.shape)
-print(y.shape)
+    # Make predictions
+    y_pred =  model.predict(X_test)
+    print(y_test)
+    print(y_pred)
+
+    # View accuracy score
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Precisión del modelo:", accuracy)
+
+    print(precision_score(y_test, y_pred, average='macro', zero_division=0))
+    print(classification_report(y_test, y_pred, zero_division=0))
+
+    #View confusion matrix
+    confusion_matrix(y_test, y_pred)
+
+get_GNB(ejemplares_c1)
