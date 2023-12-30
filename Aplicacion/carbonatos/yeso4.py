6593e05383467713337b0fd3b1155d31dc8025a2
NataliaPerez08 <natalia-perez08@ciencias.unam.mx>
2023-12-29 21:29:39 -0600

    Tabla 1- Y4

--- Aplicacion/carbonatos/yeso4.py
+++ Aplicacion/carbonatos/yeso4.py
@@ -296,130 +296,13 @@ def crear_dataframe(pigmento,tmp):
     dataframe['espectro'] = tmp.split("/")[-1]
 
     return dataframe
-def obtener_carbonato_C7_tablas_2():
-    # Info de Tabla 2
-    dir = "Espectros_FORS_2/Tablas 2/Tabla_Y4"
-    archivos=list()
-    for f in os.listdir(dir):
-        archivos.append(dir+"/"+f)
-    archivos.sort()
-    
-    df_list = list()
-
-    # A1-A5
-    tmp = archivos[0]
-    df = crear_dataframe("Resinato de cobre",tmp)
-    df_list.append(df)
-
-    tmp = archivos[1]
-    df = crear_dataframe("Amarillo de plomo (estaño. Tipo I)",tmp)
-    df_list.append(df)
-
-    tmp = archivos[2]
-    df = crear_dataframe("Amarillo de plomo (estaño. Tipo II)",tmp)
-    df_list.append(df)
-
-    tmp = archivos[3]
-    df = crear_dataframe("Ancorca. Sennelier",tmp)
-    df_list.append(df)
-
-    tmp = archivos[4]
-    df = crear_dataframe("Ancorca. Zeechi",tmp)
-    df_list.append(df)
-
-    # B1-B5
-    tmp = archivos[5]
-    df = crear_dataframe("Tierra de Sombra",tmp)
-    df_list.append(df)
-
-    tmp = archivos[6]
-    df = crear_dataframe("Sombra tostada de Chipre",tmp)
-    df_list.append(df)
-
-    tmp = archivos[7]
-    df = crear_dataframe("Siena",tmp)
-    df_list.append(df)
-
-    tmp = archivos[8]
-    df = crear_dataframe("Siena tostada",tmp)
-    df_list.append(df)
-
-    tmp = archivos[9]
-    df = crear_dataframe("Espalto",tmp)
-    df_list.append(df)
-
-    # C1-C5
-    tmp = archivos[10]
-    df = crear_dataframe("Cinabrio",tmp)
-    df_list.append(df)
-
-    tmp = archivos[11]
-    df = crear_dataframe("Almagre",tmp)
-    df_list.append(df)
-
-    tmp = archivos[12]
-    df = crear_dataframe("Azarcón o minio",tmp)
-    df_list.append(df)
-
-    tmp = archivos[13]
-    df = crear_dataframe("Cochinilla. Tlapanocheztli",tmp)
-    df_list.append(df)
-
-    tmp = archivos[14]
-    df = crear_dataframe("Alizarina",tmp)
-    df_list.append(df)
-
-    # D1-D5
-    tmp = archivos[15]
-    df = crear_dataframe("Laca rubia",tmp)
-    df_list.append(df)
-
-    tmp = archivos[16]
-    df = crear_dataframe("Azurita",tmp)
-    df_list.append(df)
-
-    tmp = archivos[17]
-    df = crear_dataframe("Esmalte",tmp)
-    df_list.append(df)
-
-    tmp = archivos[18]
-    df = crear_dataframe("Lapislazuli",tmp)
-    df_list.append(df)
-
-    tmp = archivos[19]
-    df = crear_dataframe("Indigo",tmp)
-    df_list.append(df)
-
-    # E1-E5
-    tmp = archivos[20]
-    df = crear_dataframe("Resinato de cobre",tmp)
-    df_list.append(df)
-
-    tmp = archivos[21]
-    df = crear_dataframe("Malaquita africana",tmp)
-    df_list.append(df)
-
-    tmp = archivos[22]
-    df = crear_dataframe("Tierra verde de bohemia",tmp)
-    df_list.append(df)
-
-    tmp = archivos[23]
-    df = crear_dataframe("Negro de huesos",tmp)
-    df_list.append(df)
-
-    tmp = archivos[24]
-    df = crear_dataframe("Negro de humo",tmp)
-    df_list.append(df)
-
-    return df_list
-
 
 def obtener_yeso_Y4_tablas_2():
     # Info de Tabla 1
     dir = "Espectros_FORS_2/Tablas 2/Tabla_Y4"
     archivos=list()
     for f in os.listdir(dir):
-        archivos.append(f)
+        archivos.append(dir+"/"+f)
     archivos.sort()
 
     df_list = list()
@@ -445,23 +328,23 @@ def obtener_yeso_Y4_tablas_2():
     df_list.append(df)
 
     # B1-B5
-    tmp = archivos[10]
+    tmp = archivos[5]
     df = crear_dataframe("Pardo antilope",tmp)
     df_list.append(df)
 
-    tmp = archivos[11]
+    tmp = archivos[6]
     df = crear_dataframe("Sombra tostada de Chipre",tmp)
     df_list.append(df)
 
-    tmp = archivos[12]
+    tmp = archivos[7]
     df = crear_dataframe("Ocre aleman oscuro",tmp)
     df_list.append(df)
 
-    tmp = archivos[13]
+    tmp = archivos[8]
     df = crear_dataframe("Tierra de Siena tostada oscura",tmp)
     df_list.append(df)
 
-    tmp = archivos[14]
+    tmp = archivos[9]
     df = crear_dataframe("Betun de Judea",tmp)
     df_list.append(df)
 
@@ -532,32 +415,11 @@ def obtener_yeso_Y4_tablas_2():
 
     return df_list
 
-def crear_dataframe(pigmento,tmp):
-    carpeta =  "Tablas2"
-    base = "Yeso"
-    tabla = "Y4"
-    # Crear dataframe
-    dataframe = pd.DataFrame(columns=['wavelength','reflectance','pigmento','aglutinante','base','path','carpeta','tabla','espectro'])
-
-    tmp_df = rr.create_df_from_txt(tmp)
-
-    dataframe['wavelength'] = tmp_df['Wavelength']
-    dataframe['reflectance'] = tmp_df['reflectance']
-    dataframe['pigmento'] = pigmento
-    dataframe['path'] = tmp
-    dataframe['base'] = base
-    dataframe['carpeta'] = carpeta
-    dataframe['tabla'] = tabla
-    dataframe['aglutinante'] = ""
-    dataframe['espectro'] = tmp.split("/")[-1]
-
-    return dataframe
-
 def imprimir():
-    espectros_1 = obtener_yeso_Y4_tablas_1()
-    #espectros_2 = obtener_yeso_Y4_tablas_2()
+    #espectros_1 = obtener_yeso_Y4_tablas_1()
+    espectros_2 = obtener_yeso_Y4_tablas_2()
     #espectros = espectros_1 + espectros_2
-    for o in espectros_1:
+    for o in espectros_2:
         print(o['carpeta'][0]+" "+o['tabla'][0]+" "+o['espectro'][0]+" "+o['pigmento'][0]+" "+o['aglutinante'][0]+" "+o['base'][0])
 
 imprimir()
