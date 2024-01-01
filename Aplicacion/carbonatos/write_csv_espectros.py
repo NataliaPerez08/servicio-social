54ca0c8fb18058d64b49a0494a2619cd5f7d0c01
NataliaPerez08 <natalia-perez08@ciencias.unam.mx>
2024-01-01 14:03:55 -0600

    Avances write_csv

--- Aplicacion/carbonatos/write_csv_espectros.py
+++ Aplicacion/carbonatos/write_csv_espectros.py
@@ -16,54 +16,64 @@ import yeso6 as y6
 import yeso7 as y7
 
 def obtener_cadena(ejemplares):
-    s_ejemplar = ""
+    l_obj = list() 
     for f in ejemplares:
         for o in f:
-            print(o['carpeta'][0]+","+o['tabla'][0]+","+o['espectro'][0]+","+o['pigmento'][0]+","+o['aglutinante'][0]+","+o['base'][0])
-                  
-            s_ejemplar+=o['carpeta'][0]+","+o['tabla'][0]+","+o['espectro'][0]+","+o['pigmento'][0]+","+o['aglutinante'][0]+","+o['base'][0]+"\n"
-    return s_ejemplar   
-
-
-columnas = ['Carpeta','Tabla','Espectro','Pigmento','Aglutinante','Base de preparación']
+            # ['Carpeta','Tabla','Espectro','Pigmento','Aglutinante','Base de preparación']
+            s_ejemplar = {'Carpeta':o['carpeta'][0],
+                          'Tabla':o['tabla'][0],
+                          'Espectro':o['espectro'][0],
+                          'Pigmento':o['pigmento'][0],
+                          'Aglutinante':o['aglutinante'][0],
+                          'Base de preparación':o['base'][0]}
+            l_obj.append(s_ejemplar)
+            #s_ejemplar+=o['carpeta'][0]+","+o['tabla'][0]+","+o['espectro'][0]+","+o['pigmento'][0]+","+o['aglutinante'][0]+","+o['base'][0]+"\n"
+    return l_obj
 
 str_c1 = obtener_cadena(c1.obtener_carbonato_C1())
-
-""""
 str_c2 = obtener_cadena(c2.obtener_carbonato_C2())
 str_c3 = obtener_cadena(c3.obtener_carbonato_C3())
 str_c4 = obtener_cadena(c4.obtener_carbonato_C4())
 str_c5 = obtener_cadena(c5.obtener_carbonato_C5())
 str_c6 = obtener_cadena(c6.obtener_carbonato_C6())
 
-str_c7 = c7.str_carbonato_C7()
+#str_c7 = c7.str_carbonato_C7()
 
 str_y1 = obtener_cadena(y1.obtener_yeso_Y1())
 str_y2 = obtener_cadena(y2.obtener_yeso_Y2())
 str_y3 = obtener_cadena(y3.obtener_yeso_Y3())
-str_y4 = y4.str_yeso_y4()
+#str_y4 = y4.str_yeso_y4()
 
 str_y5 = obtener_cadena(y5.obtener_yeso_Y5())
 str_y6 = obtener_cadena(y6.obtener_yeso_Y6())
-str_y7 = y7.str_yeso_y7()
-"""
+#str_y7 = y7.str_yeso_y7()
 
 print("Escribiendo espectros en csv")
 with open('espectros.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(columnas)
-    writer.writerow(str_c1)
-    #writer.writerow(str_c2)
-    #writer.writerow(str_c3)
-    #writer.writerow(str_c4)
-    #writer.writerow(str_c5)
-    #writer.writerow(str_c6)
-    #writer.writerow(str_c7)
-    #writer.writerow(str_y1)
-    #writer.writerow(str_y2)
-    #writer.writerow(str_y3)
-    #writer.writerow(str_y4)
-    #writer.writerow(str_y5)
-    #writer.writerow(str_y6)
-    #writer.writerow(str_y7)
+    fieldnames = ['Carpeta','Tabla','Espectro','Pigmento','Aglutinante','Base de preparación']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for i in str_c1:
+        writer.writerow(i)
+    for i in str_c2:
+        writer.writerow(i)
+    for i in str_c3:
+        writer.writerow(i)
+    for i in str_c4:
+        writer.writerow(i)
+    for i in str_c5:
+        writer.writerow(i)
+    for i in str_c6:
+        writer.writerow(i)
+    for i in str_y1:
+        writer.writerow(i)
+    for i in str_y2:
+        writer.writerow(i)
+    for i in str_y3:
+        writer.writerow(i)
+    for i in str_y5:
+        writer.writerow(i)
+    for i in str_y6:
+        writer.writerow(i)
 
