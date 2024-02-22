af8b0e87f0551436b64549c733f2271545426816
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-02-21 18:55:28 -0600

    Correccion. Creacion de setup

--- Aplicacion/setup.py
+++ Aplicacion/setup.py
@@ -14,10 +14,10 @@ def activate_virtual_environment(venv_path,app_path):
     # Activate the virtual environment
     try:
         if sys.platform == 'win32':
-            activate_script = os.path.join(venv_path, 'Scripts', 'activate')
-            activate_command = f"{activate_script}"
-            call(activate_command, shell=True)
-            print(f"Activated virtual environment at {venv_path}")
+            #activate_script = os.path.join(venv_path, 'Scripts', 'activate')
+            #activate_command = f"{activate_script}"
+            #call(activate_command, shell=True)
+            #print(f"Activated virtual environment at {venv_path}")
 
             pip_command = os.path.join(venv_path, 'Scripts', 'pip')
 
@@ -27,7 +27,15 @@ def activate_virtual_environment(venv_path,app_path):
             print(path_req)
 
             call(f"{pip_command} install -r {path_req}", shell=True)
-           
+
+            print("Requirements installed. Running the application... 1. Clasificador 2. Organizador")
+            option=int(input())
+            if(option==1):
+                combine = os.path.join(venv_path, 'Scripts', 'python') + " clasificador.py"
+                call(combine, shell=True)
+            else:
+                combine = os.path.join(venv_path, 'Scripts', 'python') + " organizador.py"
+                call(combine, shell=True)
         else:
             activate_script = os.path.join(venv_path, 'bin', 'activate')
             activate_command = f"source {activate_script}"
