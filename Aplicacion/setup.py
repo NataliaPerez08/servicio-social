49a0ae341f83d625e6f3c1decd8b11bcd32d824e
NataliaPerez08 <natalia-perez08@ciencias.unam.mx>
2024-02-28 12:03:12 -0600

    Linux Version

--- Aplicacion/setup.py
+++ Aplicacion/setup.py
@@ -14,10 +14,7 @@ def activate_virtual_environment(venv_path,app_path):
     # Activate the virtual environment
     try:
         if sys.platform == 'win32':
-            #activate_script = os.path.join(venv_path, 'Scripts', 'activate')
-            #activate_command = f"{activate_script}"
-            #call(activate_command, shell=True)
-            #print(f"Activated virtual environment at {venv_path}")
+            # Windows
 
             pip_command = os.path.join(venv_path, 'Scripts', 'pip')
 
@@ -37,12 +34,19 @@ def activate_virtual_environment(venv_path,app_path):
                 combine = os.path.join(venv_path, 'Scripts', 'python') + " organizador.py"
                 call(combine, shell=True)
         else:
-            activate_script = os.path.join(venv_path, 'bin', 'activate')
-            activate_command = f"source {activate_script}"
+            # Linux
+            pip_command = os.path.join(venv_path, 'bin', 'pip')
+            print(f"Installing requirements using {pip_command}")
+            call(f"{pip_command} install -r requi.txt", shell=True)
+            print("Requirements installed. Running the application... 1. Clasificador 2. Organizador")
+            option=int(input())
+            if(option==1):
+                combine = os.path.join(venv_path, 'bin', 'python') + " clasificador.py"
+                call(combine, shell=True)
+            else:
+                combine = os.path.join(venv_path, 'bin', 'python') + " organizador.py"
+                call(combine, shell=True)
 
-            call(activate_command, shell=True)
-            print(f"Activated virtual environment at {venv_path}")
-            install_requirements(venv_path)
     except Exception as e:
         print(f"Error activating virtual environment: {e}")
 
